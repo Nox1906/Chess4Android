bd20aa09da75aa21b7194a09ea9d8cc58263a9b5
TiagoRibeiro47612 <a47612@alunos.isel.pt>
2021-11-07 21:54:50 +0000

    add touchListener version

--- Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/views/BoardView.kt
+++ Chess4Android/app/src/main/java/pt/isel/pdm/chess4android/views/BoardView.kt
@@ -7,23 +7,19 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.GridLayout
 import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
+import pt.isel.pdm.chess4android.Army
+import pt.isel.pdm.chess4android.Piece
 import pt.isel.pdm.chess4android.R
 import pt.isel.pdm.chess4android.views.Tile.Type
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.net.URL
+
+
+typealias TileTouchListener = (tile: Tile, row: Int, column: Int) -> Unit
 
 /**
  * Custom view that implements a chess board.
  */
 @SuppressLint("ClickableViewAccessibility")
 class BoardView(private val ctx: Context, attrs: AttributeSet?) : GridLayout(ctx, attrs) {
-
-
-    var retrofit = Retrofit.Builder()
-        .baseUrl("https://lichess.org/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 
     private val side = 8
 
@@ -33,17 +29,42 @@ class BoardView(private val ctx: Context, attrs: AttributeSet?) : GridLayout(ctx
         strokeWidth = 10F
     }
 
-    init {
+    private fun createImageEntry(army: Army, piece: Piece, imageId: Int) =
+        Pair(Pair(army, piece), VectorDrawableCompat.create(ctx.resources, imageId, null))
 
+    private val piecesImages = mapOf(
+        createImageEntry(Army.WHITE, Piece.PAWN, R.drawable.ic_white_pawn),
+        createImageEntry(Army.WHITE, Piece.KNIGHT, R.drawable.ic_white_knight),
+        createImageEntry(Army.WHITE, Piece.BISHOP, R.drawable.ic_white_bishop),
+        createImageEntry(Army.WHITE, Piece.ROOK, R.drawable.ic_white_rook),
+        createImageEntry(Army.WHITE, Piece.QUEEN, R.drawable.ic_white_queen),
+        createImageEntry(Army.WHITE, Piece.KING, R.drawable.ic_white_king),
+        createImageEntry(Army.BLACK, Piece.PAWN, R.drawable.ic_black_pawn),
+        createImageEntry(Army.BLACK, Piece.KNIGHT, R.drawable.ic_black_knight),
+        createImageEntry(Army.BLACK, Piece.BISHOP, R.drawable.ic_black_bishop),
+        createImageEntry(Army.BLACK, Piece.ROOK, R.drawable.ic_black_rook),
+        createImageEntry(Army.BLACK, Piece.QUEEN, R.drawable.ic_black_queen),
+        createImageEntry(Army.BLACK, Piece.KING, R.drawable.ic_black_king),
+    )
+
+    init {
         rowCount = side
         columnCount = side
         repeat(side * side) {
             val row = it / side
             val column = it % side
-            val tile = Tile(ctx, if ((row + column) % 2 == 0) Type.BLACK else Type.WHITE, side)
+            val tile = Tile(
+                ctx,
+                if((row + column) % 2 == 0) Type.WHITE else Type.BLACK,
+                side,
+                piecesImages
+            )
+            tile.setOnClickListener { onTileClickedListener?.invoke(tile, row, column) }
             addView(tile)
         }
     }
+
+    var onTileClickedListener: TileTouchListener? = null
 
     override fun dispatchDraw(canvas: Canvas) {
         super.dispatchDraw(canvas)
